a553dd611b5d4be250983e28ca7cb59ffdd13543
GetSomeSleepBro <152909604+GetSomeSleepBro@users.noreply.github.com>
2024-07-15 15:10:51 +0000

    Update GroupA_Assignment1.py

--- FDS/GroupA_Assignment1.py
+++ FDS/GroupA_Assignment1.py
@@ -9,12 +9,16 @@ Experiment No. 1 : In a second year computer engineering class, group A students
 (NOTE : While realising the group, duplicate entries should be avoided. Do not use SET built-in functions)
 '''
 
+#<------------------------------------------------------------------------------------------------->
+
 def remove_duplicates(lst):
     new_lst = []
     for i in lst:
         if i not in new_lst:
             new_lst.append(i)
     return new_lst
+
+#<------------------------------------------------------------------------------------------------->
 
 def intersection(lst1, lst2):
     new_lst = []
@@ -23,12 +27,16 @@ def intersection(lst1, lst2):
             new_lst.append(val)
     return new_lst
 
+#<------------------------------------------------------------------------------------------------->
+
 def union(lst1, lst2):
     new_lst = lst1.copy()
     for val in lst2:
         if val not in new_lst:
             new_lst.append(val)
     return new_lst
+
+#<------------------------------------------------------------------------------------------------->
 
 def difference(lst1, lst2):
     new_lst = []
@@ -37,13 +45,16 @@ def difference(lst1, lst2):
             new_lst.append(val)
     return new_lst
 
+#<------------------------------------------------------------------------------------------------->
+
 def symmetric_difference(lst1,lst2):
     lst3=[]
     D1=difference(lst1,lst2)
     D2=difference(lst2,lst1)
     lst3=union(D1,D2)
     return lst3
-    
+
+#<------------------------------------------------------------------------------------------------->
     
 def main():
     se_comp = input("Enter names of students in SE COMP (separated by space): ").split()
@@ -74,11 +85,16 @@ def main():
         else:
             print("!!Wrong Choice!! ")
 
+#<------------------------------------------------------------------------------------------------->
+
 main()
 
+#<------------------------------------------ END OF PROGRAM ----------------------------------------->
 
-''' OUTPUT:
 
+
+#<-------------------------------------------- OUTPUT ----------------------------------------------->
+''' 
 Enter names of students in SE COMP (separated by space): a b c d e f g
 Enter names of students who play cricket (separated by space): a b c
 Enter names of students who play football (separated by space): c d f

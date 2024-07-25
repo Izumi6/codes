f271861e4e27369dfc77966c0fcd9500325f2f01
GetSomeSleepBro <152909604+GetSomeSleepBro@users.noreply.github.com>
2024-07-25 10:36:41 +0530

    Add files via upload

---
+++ मम/TCP_localchat/client.py
@@ -0,0 +1,54 @@
+# --------------------------------------------------------------
+# 							client.py
+# --------------------------------------------------------------
+
+# req. libs
+import socket
+import threading
+
+
+# global
+## connection data
+host = '127.0.0.1' # server ip
+port = 4444        # free/open port
+print(f"[#] connecting to {host}:{port}")
+
+## nickname
+nickname = input("[+] enter nickname:")
+
+## starting client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+print(f"[+] connected to {host}:{port}")
+
+
+# listening to server & sending nickname
+def receive():
+    while True:
+        try:
+            # receive msg
+            msg = client.recv(1024).decode('ascii')
+
+            # print msg
+            if msg == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(msg)
+        except:
+            print("[!] error 404\n[#] terminating...")
+            client.close()
+            break
+
+# send msg to server
+def write():
+    while True:
+        msg = f"{nickname}: {input('')}"
+        client.send(msg.encode('ascii'))
+
+
+# starting threads for listening & writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ मम/TCP_localchat/server.py
@@ -0,0 +1,95 @@
+# TCP (lan) based chatting app
+
+# ref:
+# - https://www.neuralnine.com/tcp-chat-in-python/ 
+# - https://www.geeksforgeeks.org/simple-chat-room-using-python/
+
+
+# --------------------------------------------------------------
+# 							server.py
+# --------------------------------------------------------------
+
+
+# req. libs
+import socket
+import threading
+
+
+
+# global
+## connection data
+host = '127.0.0.1' # server ip
+port = 4444        # free/open port
+
+## starting server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# AF_INET - using internal socket, rather than unix socket
+	# SOCK_STREAM - using tcp (& not udp)
+server.bind((host,port))
+server.listen()
+
+## clients & nicknames
+clients = []
+nicknames = []
+
+print(f"[+] Server is running on {host}:{port}")
+
+
+# send msg to all clients
+def broadcast(msg):
+	for client in clients:
+		client.send(msg)
+
+
+# handling clients 
+def handle(client):
+	while True:
+		try:
+			msg = client.recv(1024)
+			broadcast(msg)
+		except:
+			# disconnecting inactive clients
+			index = clients.index(client)
+
+			# del f/ clients
+			try: clients.remove(index)
+			except: pass
+			client.close()
+
+			# del f/ nicknames
+			nickname = nicknames.index(index)
+			nicknames.remove(nickname)
+
+			# display
+			print(f"[-] '{nickname}' left")
+			break
+
+
+# listening
+def receive():
+	while True:
+		# accept conn
+		client, addr = server.accept()
+		print(f"[+] '{str(addr)}' connected")
+
+		# request & store nickname
+		client.send('NICKNAME'.encode('ascii'))
+		nickname = client.recv(1024).decode('ascii')
+
+		# store
+		clients.append(client)
+		nicknames.append(nickname)
+
+		# display
+		print(f"[+] '{nickname}' connected")
+
+		# send msg
+		broadcast(f"[#] {nickname} connected to the server\n".encode('ascii'))
+
+		# handle
+		client_handler = threading.Thread(target=handle, args=(client,))
+		client_handler.start()
+
+
+receive()
+		

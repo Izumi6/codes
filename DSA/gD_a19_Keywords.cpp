851da3e7f6a7e0712b46e1e3021b6dbb920a58de
Sairaj Jawalikar <152909604+GetSomeSleepBro@users.noreply.github.com>
2025-04-26 07:12:35 +0530

    Update gD_a19_Keywords.cpp

--- DSA/gD_a19_Keywords.cpp
+++ DSA/gD_a19_Keywords.cpp
@@ -1,3 +1,52 @@
+/*
+QUESTION:
+    A Dictionary stores keywords and its meanings. Provide facility for adding new
+    keywords, deleting keywords, updating values of any entry. Provide facility to display
+    whole data sorted in ascending/ Descending order. Also find how many maximum
+    comparisons may require for finding any keyword. Use Height balance tree and find the
+    complexity for finding a keyword
+
+OUTPUT:
+    --- Dictionary using AVL Tree ---
+    1. Add Keyword
+    2. Delete Keyword
+    3. Update Meaning
+    4. Display in Ascending Order
+    5. Display in Descending Order
+    6. Search Keyword
+    7. Exit
+    Enter your choice: 1
+    Enter keyword: HI
+    Enter meaning: HELLO
+    
+    --- Dictionary using AVL Tree ---
+    1. Add Keyword
+    2. Delete Keyword
+    3. Update Meaning
+    4. Display in Ascending Order
+    5. Display in Descending Order
+    6. Search Keyword
+    7. Exit
+    Enter your choice: 1
+    Enter keyword: BYE
+    Enter meaning: BYEE
+    
+    --- Dictionary using AVL Tree ---
+    1. Add Keyword
+    2. Delete Keyword
+    3. Update Meaning
+    4. Display in Ascending Order
+    5. Display in Descending Order
+    6. Search Keyword
+    7. Exit
+    Enter your choice: 4
+    
+    Dictionary in Ascending Order:
+    BYE : BYEE
+    HI : HELLO
+*/
+    
+
 #include <iostream>
 using namespace std;
 
